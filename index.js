ca3062d7649331eee0f8724a213bc571d1fe4328
ivanpr87 <ivanbastos18@gmail.com>
2022-06-16 21:42:55 -0300

    termine desafio1

---
+++ index.js
@@ -0,0 +1,7 @@
+let perrito = prompt("ingresar un nombre  o escribe ESC para salir")
+while (perrito != "ESC") {
+
+    alert("el usuario ingreso el nombre de " + perrito);
+    perrito = prompt("ingresa otro nombre o escribe ESC para salir ");
+
+}
